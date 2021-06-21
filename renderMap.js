c8ae48181ef2b30eef1a4bbaed60d39addf467c0
punnbaza <85600706+punnbaza@users.noreply.github.com>
2021-06-22 04:38:13 +0700

    Update renderMap.js

--- renderMap.js
+++ renderMap.js
@@ -14,92 +14,109 @@ function Map(props) {
             css:true
         }).then(([MapView,WebMap,GeoJSONLayer])=>{
             const webmap = new WebMap({
-                basemap: 'topo-vector'
+                basemap: 'satellite'
             })
             view = new MapView({
                 map: webmap,
-                center: [100.15 ,13.8],
+                center: [100.31,13.74],
                 zoom: 12,
                 container : MapEl.current
             })
+            /*const geojsonLayer = new GeoJSONLayer({
+                url: "https://raw.github.com/chtoeii/Geojson-Data/blob/main/pipe_line.geojson"
+                        });
+            webmap.add(geojsonLayer)*/
             
-            if(myId === "Map202006"){
-                console.log('06');
+            if(myId === "June2020"){
+                
                 const geojsonLayer = new GeoJSONLayer({
                     url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202006_lalong.geojson"
                             });
                 webmap.add(geojsonLayer)  
             }
-            else if(myId === "Map202007"){
-                console.log('07');
+            else if(myId === "July2020"){
+                
                 const geojsonLayer = new GeoJSONLayer({
                     url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202007_lalong.geojson"
                             });
                 webmap.add(geojsonLayer)  
             }    
-            else if(myId === "Map202008"){
+            else if(myId === "August2020"){
+                
                 const geojsonLayer = new GeoJSONLayer({
                     url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202008_lalong.geojson"
                             });
                 webmap.add(geojsonLayer)  
             }
-            else if(myId === "Map202009"){
+            else if(myId === "September2020"){
+                
                 const geojsonLayer = new GeoJSONLayer({
                     url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202009_lalong.geojson"
                             });
                 webmap.add(geojsonLayer)  
             }
-            else if(myId === "Map202010"){
+            else if(myId === "October2020"){
+                
                 const geojsonLayer = new GeoJSONLayer({
                     url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202010_lalong.geojson"
                             });
                 webmap.add(geojsonLayer)  
             }
-            else if(myId === "Map202011"){
+            else if(myId === "November2020"){
+                
                 const geojsonLayer = new GeoJSONLayer({
                     url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202011_lalong.geojson"
                             });
                 webmap.add(geojsonLayer)  
             }
-            else if(myId === "Map202012"){
+            else if(myId === "December2020"){
+                
                 const geojsonLayer = new GeoJSONLayer({
                     url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202012_lalong.geojson"
                             });
                 webmap.add(geojsonLayer)  
             }
-            else if(myId === "Map202101"){
+            else if(myId === "January2021"){
+                
                 const geojsonLayer = new GeoJSONLayer({
                     url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202101_lalong.geojson"
                             });
                 webmap.add(geojsonLayer)  
             }
-            else if(myId === "Map202102"){
+            else if(myId === "February2021"){
+                
                 const geojsonLayer = new GeoJSONLayer({
                     url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202102_lalong.geojson"
                             });
                 webmap.add(geojsonLayer)  
             }
-            else if(myId === "Map202103"){
+            else if(myId === "March2021"){
+                
                 const geojsonLayer = new GeoJSONLayer({
                     url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202103_lalong.geojson"
                             });
                 webmap.add(geojsonLayer)  
             } 
-            else if(myId === "Map202104"){
+            else if(myId === "April2021"){
+                
                 const geojsonLayer = new GeoJSONLayer({
                     url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202104_lalong.geojson"
                             });
                 webmap.add(geojsonLayer)  
-            }                       
+            }
+                                 
             else{
+                const geojsonLayer = new GeoJSONLayer({
+                    url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/pipe_line.geojson"
+                            });
+                webmap.add(geojsonLayer)
             
             }
-            /*
-            const geojsonLayer = new GeoJSONLayer({
-                url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202006_lalong.geojson"
-                        });
-            webmap.add(geojsonLayer)
-            */
+
+            //url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/pipe_line.geojson"   
+            //webmap.add(geojsonLayer)
+            
+
         })
         return()=>{
             if(!!view){
